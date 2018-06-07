5cceb2788a4741346ae7e097d29af1e31e40342d
pbartkowicz <37899252+pbartkowicz@users.noreply.github.com>
2018-06-07 19:43:55 +0200

    Update Kingdom.java

--- src/Kingdom.java
+++ src/Kingdom.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Kingdom {
+    public String name;
+    public King king;
+    private List<Senior> seniors;
+
+    Kingdom(String name, King king) {
+        this.name = name;
+        this.king = king;
+    }
+
+    public List<Senior> getSeniors() {
+        return seniors;
+    }
 }
